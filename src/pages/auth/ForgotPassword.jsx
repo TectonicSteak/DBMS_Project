3531e221f63996e2f3b4b371d60f7c2c1b8020ba
TectonicSteak <adithyasp2003@gmail.com>
2023-11-19 10:52:52 +0530

    updated files

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -10,7 +10,7 @@ const ForgotPassword = () => {
     e.preventDefault();
 
     try {
-      const { error } = await supabase.auth.resetPasswordForEmail(email,{redirectTo: `https://student-management-dbms-app.netlify.app/reset_password`});
+      const { error } = await supabase.auth.resetPasswordForEmail(email,{redirectTo: `https://student-management-dbms.vercel.app/reset_password`});
 
       if (error) {
         throw error;
