b221ddb6fbece67d944c8b9b69a4acea4dac4fd6
Gourisankar Shaji <gourisankar.shaji@gmail.com>
2023-11-22 22:40:10 +0530

    improved css on the signup page

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -16,15 +16,15 @@ const SignUp = () => {
   const [teacherId, setTeacherId] = useState('');
   const [Class, setClass] = useState('');
   const [semester, setSemester] = useState('');
-  const [rollno , setRollno] = useState();
-  const [dept,setDept] = useState('');
-  const departments = [[1,'Computer Science and Engineering'],
-                       [2,'Electrical and Electronics Engineering'],
-                       [3,'Electronics and Communcation Engineering'],
-                       [4,'Electronics and Biomedical Engineering']
-                      ];
+  const [rollno, setRollno] = useState();
+  const [dept, setDept] = useState('');
+  const departments = [[1, 'Computer Science and Engineering'],
+  [2, 'Electrical and Electronics Engineering'],
+  [3, 'Electronics and Communcation Engineering'],
+  [4, 'Electronics and Biomedical Engineering']
+  ];
 
-  const classes = ['A','B','C','U']
+  const classes = ['A', 'B', 'C', 'U']
 
   const handleSignup = async (e) => {
     e.preventDefault();
@@ -45,7 +45,7 @@ const SignUp = () => {
       return;
     }
 
-    if(password.length < 6){
+    if (password.length < 6) {
       toast.error("Password must be having 6 or more characters", {
         autoClose: 1500,
         position: "bottom-right"
@@ -135,35 +135,49 @@ const SignUp = () => {
   };
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-slate-300">
-      <h1 className="text-5xl font-semibold mb-5">Sign Up</h1>
-      <form className="w-1/3" onSubmit={handleSignup}>
-        <TextInput
-          label="Name"
-          value={name}
-          function={setName}
-        />
-
-        <TextInput
-          type="email"
-          label="Email"
-          value={email}
-          function={setEmail}
-        />
-
-        <TextInput
-          type="password"
-          label="Password"
-          value={password}
-          function={setPassword}
-        />
+    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-300">
+      <h1 className="text-6xl font-bold text-gray-700 mb-8">Sign Up</h1>
+      <form className="w-full max-w-lg p-8 space-y-6 bg-white rounded-lg shadow-md" onSubmit={handleSignup}>
 
         <div className="mb-4">
-          <label className="text-lg font-semibold  mb-2">User Type</label>
+          <label className="block text-xl font-medium text-gray-600 mb-2">Name</label>
+          <input
+            type="text"
+            className="w-full px-4 py-3 border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-200"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-xl font-medium text-gray-600 mb-2">Email</label>
+          <input
+            type="email"
+            className="w-full px-4 py-3 border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-200"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-xl font-medium text-gray-600 mb-2">Password</label>
+          <input
+            type="password"
+            className="w-full px-4 py-3 border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-200"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-xl font-medium text-gray-600 mb-2">User Type</label>
           <select
             value={userType}
             onChange={(e) => setUserType(e.target.value)}
-            className="w-full px-3 py-2 border rounded outline-none"
+            className="w-full px-4 py-3 border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-200"
           >
             <option value="student">Student</option>
             <option value="teacher">Teacher</option>
@@ -172,85 +186,90 @@ const SignUp = () => {
 
         {userType === 'student' && (
           <div>
-            <TextInput
-              label="Registration Number"
-              value={registrationNumber}
-              function={setRegistrationNumber}
-            />
-
-            <TextInput
-              type="number"
-              label="Roll No"
-              value={rollno}
-              function={setRollno}
-            />
-
             <div className="mb-4">
-                <label className="block text-lg font-semibold mb-2">
-                    Class:
-                </label>
-                <select
-                    value={Class}
-                    onChange={(e) => setClass(e.target.value)}
-                    className="border rounded-md p-2 w-full"
-                    >
-                    <option value="" disabled>Select Class</option>
-                    {classes.map((cls) => (
-                        <option key={cls} value={cls}>{cls}</option>
-                    ))}
-                </select>
+              <label className="block text-xl font-medium text-gray-600 mb-2">Registration Number</label>
+              <input
+                type="text"
+                className="w-full px-4 py-3 border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-200"
+                value={registrationNumber}
+                onChange={(e) => setRegistrationNumber(e.target.value)}
+              />
             </div>
 
             <div className="mb-4">
-                <label className="block text-lg font-semibold mb-2">
-                    Semester:
-                </label>
-                <select
-                    value={semester}
-                    onChange={(e) => setSemester(e.target.value)}
-                    className="border rounded-md p-2 w-full"
-                    >
-                    <option value="" disabled>Select Semester</option>
-                    {[1,2,3,4,5,6,7,8].map((sem) => (
-                        <option key={sem} value={sem}>{sem}</option>
-                    ))}
-                </select>
+              <label className="block text-xl font-medium text-gray-600 mb-2">Roll No</label>
+              <input
+                type="number"
+                className="w-full px-4 py-3 border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-200"
+                value={rollno}
+                onChange={(e) => setRollno(e.target.value)}
+              />
             </div>
 
             <div className="mb-4">
-              <label className="block text-lg font-semibold mb-2">
-                  Department:
-              </label>
+              <label className="block text-xl font-medium text-gray-600 mb-2">Class:</label>
               <select
-                  value={dept}
-                  onChange={(e) => setDept(e.target.value)}
-                  className="border rounded-md p-2 w-full"
-                  >
-                  <option value="" disabled>Select Department</option>
-                  {departments.map((dept) => (
-                      <option key={dept[0]} value={dept[0]}>{dept[1]}</option>
-                  ))}
+                value={Class}
+                onChange={(e) => setClass(e.target.value)}
+                className="w-full px-4 py-3 border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-200"
+              >
+                <option value="" disabled>Select Class</option>
+                {classes.map((cls) => (
+                  <option key={cls} value={cls}>{cls}</option>
+                ))}
+              </select>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-xl font-medium text-gray-600 mb-2">Semester:</label>
+              <select
+                value={semester}
+                onChange={(e) => setSemester(e.target.value)}
+                className="w-full px-4 py-3 border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-200"
+              >
+                <option value="" disabled>Select Semester</option>
+                {[1, 2, 3, 4, 5, 6, 7, 8].map((sem) => (
+                  <option key={sem} value={sem}>{sem}</option>
+                ))}
+              </select>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-xl font-medium text-gray-600 mb-2">Department:</label>
+              <select
+                value={dept}
+                onChange={(e) => setDept(e.target.value)}
+                className="w-full px-4 py-3 border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-200"
+              >
+                <option value="" disabled>Select Department</option>
+                {departments.map((dept) => (
+                  <option key={dept[0]} value={dept[0]}>{dept[1]}</option>
+                ))}
               </select>
             </div>
           </div>
         )}
 
         {userType === 'teacher' && (
-          <TextInput
-            label="Teacher ID"
-            value={teacherId}
-            function={setTeacherId}
-          />
+          <div className="mb-4">
+            <label className="block text-xl font-medium text-gray-600 mb-2">Teacher ID</label>
+            <input
+              type="text"
+              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-200"
+              value={teacherId}
+              onChange={(e) => setTeacherId(e.target.value)}
+            />
+          </div>
         )}
 
         <button
           type="submit"
-          className="w-full bg-blue-500 hover:bg-blue-700 font-semibold text-white py-2 px-4 rounded cursor-pointer"
+          className="w-full bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-semibold py-3 px-4 rounded cursor-pointer transition duration-300"
         >
           Sign Up
         </button>
       </form>
-      <Link to='/' className="text-blue-500 font-semibold mt-4 cursor-pointer">
+      <Link to='/' className="text-blue-500 font-medium mt-5 hover:text-blue-600 transition duration-300">
         Already Registered?
       </Link>
       <ToastContainer
